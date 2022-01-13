7e5b673fb92855ba9cfb66fd294b3e5077097444
Liam Ederzeel <liamederzeel@gmail.com>
2022-01-13 22:12:20 +0100

    refactor: performance

--- Pool.cs
+++ Pool.cs
@@ -4,47 +4,44 @@
 	using System.Collections.Generic;
 
 	public class Pool<T> : IEnumerable where T : IResettable {
+		private Stack<T> _availableMembers = new Stack<T>();
+		private Dictionary<int, T> _unavailableMembers = new Dictionary<int, T>();
+		private IFactory<T> _factory;
 
-		public List<T> members = new List<T>();
-		public HashSet<T> unavailable = new HashSet<T>();
-		IFactory<T> factory;
-
-		public Pool(IFactory<T> factory) : this(factory, 5) { }
+		public Pool(IFactory<T> factory) : this(factory, 0) { }
 
 		public Pool(IFactory<T> factory, int poolSize) {
-			this.factory = factory;
+			_factory = factory;
 
 			for(int i = 0; i < poolSize; i++) {
-				Create();
+				_availableMembers.Push(Create());
 			}
 		}
 
 		public T Allocate() {
-			for(int i = 0; i < members.Count; i++) {
-				if(!unavailable.Contains(members[i])) {
-					unavailable.Add(members[i]);
-					return members[i];
-				}
+			if(_availableMembers.Count < 1) {
+                T newMember = Create();
+                _unavailableMembers.Add(newMember.GetHashCode(), newMember);
+                return newMember;
 			}
-			T newMember = Create();
-			unavailable.Add(newMember);
-			return newMember;
+
+			T allocatedMember = _availableMembers.Pop();
+			_unavailableMembers.Add(allocatedMember.GetHashCode(), allocatedMember);
+			return allocatedMember;
 		}
 
 		public void Release(T member) {
 			member.Reset();
-			unavailable.Remove(member);
+			_unavailableMembers.Remove(member.GetHashCode());
+			_availableMembers.Push(member);
 		}
 
 		T Create() {
-			T member = factory.Create();
-			members.Add(member);
-			return member;
+			return _factory.Create();
 		}
 
 		IEnumerator IEnumerable.GetEnumerator() {
-			return (IEnumerator<T>)members.GetEnumerator();
+			return (IEnumerator<T>)_availableMembers.GetEnumerator();
 		}
-
 	}
 }
